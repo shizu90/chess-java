f82b361f2a1292666fc89759574457b1f069b05a
Gabriel <gabriel.moretto9090@gmail.com>
2022-08-08 14:06:24 -0300

    Implementando movimentos possíveis do rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,60 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position pos) {
+		ChessPiece p = (ChessPiece)getBoard().piece(pos);
+		return p == null || p.getColor() != this.getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] tmp = new boolean[getBoard().getRows()][getBoard().getCols()];
+		
+		Position p = new Position(0, 0);
+		
+		//above
+		p.setValues(pos.getRow() - 1, pos.getCol());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//below
+		p.setValues(pos.getRow() + 1, pos.getCol());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//left
+		p.setValues(pos.getRow(), pos.getCol() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//right
+		p.setValues(pos.getRow(), pos.getCol() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//nw
+		p.setValues(pos.getRow() - 1, pos.getCol() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		
+		//ne
+		p.setValues(pos.getRow() - 1, pos.getCol() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//sw
+		p.setValues(pos.getRow() + 1, pos.getCol() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		//se
+		p.setValues(pos.getRow() + 1, pos.getCol() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			tmp[p.getRow()][p.getCol()] = true;
+		}
+		
 		return tmp;
 	}
 }
